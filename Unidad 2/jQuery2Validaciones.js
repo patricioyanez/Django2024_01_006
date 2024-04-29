ced032601b7d325c4c91e70854f387300ea38529
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-04-29 18:20:23 -0400

    js validaciones

--- Unidad 2/jQuery2Validaciones.js
+++ Unidad 2/jQuery2Validaciones.js
@@ -11,12 +11,23 @@ $(function(){
             alert("Falta especificar el correo");
             $('.txtEmail').focus();
         }
+        // validar que el correo tenga el formato correcto
+        else if(! (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test($.trim($('.txtEmail').val()))))
+        {
+            alert("El formato del correo no es correcto");
+            $('.txtEmail').focus();
+        }
         else if($.trim($('.txtPassword').val())=="")
         {
             alert("Falta especificar la clave");
             $('.txtPassword').focus();
         }
-        // validar que el correo tenga el formato correcto
+        // permitir que la clave contenga letras y numeros
+        else if(!(/^[a-zA-Z0-9]+$/.test($.trim($('.txtPassword').val()))))
+        {
+            alert("El formato de la clave no es válido");
+            $('.txtPassword').focus();
+        }
         else
         {
             alert('Los datos enviados son: \n' + $('.txtEmail').val() + ' '
