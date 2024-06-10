bb4b82defc8c6bfe7d6af9a61a3aec13344f1414
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-06-10 17:47:09 -0400

    update

--- Unidad 3/proyecto006/instituto/alumnos/models.py
+++ Unidad 3/proyecto006/instituto/alumnos/models.py
@@ -46,3 +46,11 @@ class Alumno(models.Model):
     def __str__(self):
         return self.rut + "-" + self.dv + " " + self.nombre + " " + self.apellido1 + " " + self.apellido2
 
+class Usuario(models.Model):
+    email = models.CharField(primary_key=True, max_length=100)
+    nombre      = models.CharField(max_length=50)
+    apellido1   = models.CharField(max_length=50)
+    apellido2   = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.email + " " + self.nombre + " " + self.apellido1 + " " + self.apellido2
